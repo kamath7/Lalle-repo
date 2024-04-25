40a8b24541af16f8545a3c340dc8c1dc30bd1853
kamath7 <adithyakamath96@gmail.com>
2024-04-26 02:39:46 +0530

    case class

--- Scala/Random1/src/main/scala/Main.scala
+++ Scala/Random1/src/main/scala/Main.scala
@@ -1,10 +1,13 @@
 
 object Main {
+
   def main(args: Array[String]): Unit = {
 
-    val person = new Person("Adithya", 2014)
+    val person = new Person("Adithya", 2014, Car("Porsche","Carrera 911"))
 
     println(s"Hello there ${person.getName}. Your age is ${person.getAge}")
     println(person.canSpeak())
+    println(person.getCar)
+
   }
 }
--- Scala/Random1/src/main/scala/Person.scala
+++ Scala/Random1/src/main/scala/Person.scala
@@ -1,6 +1,7 @@
-class Person (private var name: String, private var age:Int) extends Speech {
+class Person (private var name: String, private var age:Int, private var car: Car) extends Speech {
   def getName:String = name
   def getAge:Int = age
+  def getCar:String = s" ${this.name} has ${car.brandName} - ${car.modelName}"
 
   override def nameOfPerson: String = getName
 }
@@ -9,3 +10,5 @@ trait Speech {
   def nameOfPerson: String
   def canSpeak():String = s"${nameOfPerson} is speaking"
 }
+
+case class Car(brandName: String,  modelName: String)

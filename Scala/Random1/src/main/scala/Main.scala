e42764c6946d1126861ee3c8b3015f77fe4cb61b
kamath7 <adithyakamath96@gmail.com>
2024-04-26 02:25:44 +0530

    a random scala class

---
+++ Scala/Random1/src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val person = new Person("Adithya", 2014)
+
+    println(s"Hello there ${person.getName}. Your age is ${person.getAge}")
+  }
+}
---
+++ Scala/Random1/src/main/scala/Person.scala
@@ -0,0 +1,4 @@
+class Person (private var name: String, private var age:Int){
+  def getName:String = name
+  def getAge:Int = age
+}

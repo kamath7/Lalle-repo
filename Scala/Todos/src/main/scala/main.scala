69faf7d71d6723013635f8dff2037d71457ded5a
kamath7 <adithyakamath96@gmail.com>
2024-05-14 02:57:22 +0530

    more code added to support simples

--- Scala/Todos/src/main/scala/main.scala
+++ Scala/Todos/src/main/scala/main.scala
@@ -1,5 +1,8 @@
+import scala.collection.mutable
+
 @main
 def main(): Unit = {
+
 
   val sampleTodo: TodoClass = new TodoClass("Eat Dinner","Gobble up some food",100L,"Low")
 //  print(sampleTodo)
@@ -10,9 +13,30 @@ def main(): Unit = {
   val scalaSimplerTodo = TodoCreator("Chat with Seere","Silly chats with Domesh Seere",100L,"HIGH")
 //  print(scalaSimplerTodo)
 
+
+  //Using Java classes
   val daysTodayList = new TodoListJava()
   daysTodayList.addTodo(new TodoClass("Eat Dinner","Gobble up some food",100L,"Low"))
   daysTodayList.addTodo(new TodoClass("Eat Lunch", "Gobble up some food", 100L, "Low"))
   daysTodayList.addTodo(new TodoClass("Eat Breakfast", "Gobble up some food", 100L, "Low"))
   println(daysTodayList)
+
+  //The Scala Way
+
+  val scalaTodos =  TodoListScala
+  scalaTodos.addTodo(TodoCreator("Chat with Seere","Silly chats with Domesh Seere",100L,"HIGH"))
+  scalaTodos.addTodo(TodoCreator("Chat with Natesh", "Silly chats with Nanga Natesh", 100L, "HIGH"))
+  scalaTodos.addTodo(TodoCreator("Chat with Ntina","Silly chats with Protina Ntina",100L,"HIGH"))
+
+  print(scalaTodos)
+}
+
+object TodoListScala {
+  private val todos: mutable.Buffer[TodoCreator] = mutable.Buffer.empty[TodoCreator]
+
+  def addTodo(todo: TodoCreator): Unit = {
+    todos += todo
+  }
+
+  override def toString: String = todos.mkString("\n")
 }
